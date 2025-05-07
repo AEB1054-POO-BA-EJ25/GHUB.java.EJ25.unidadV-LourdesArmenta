bd96ac79928483034a379d5d582ec3bc5ae22d7d
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-05-07 14:59:33 +0000

    rutina de validacion

--- miPrincipal/TestException.java
+++ miPrincipal/TestException.java
@@ -1,14 +1,35 @@
 package miPrincipal;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class TestException {
     public static void main(String[] args) {
-        try{
-        
-        int resultado = 10/0;
-        System.out.println(resultado);
-        } catch (ArithmeticException ex){
-            System.out.println("Error: se intentó dividir entre cero"+ex.getMessage());
+        Scanner entrada = new Scanner(System.in);
+          
+        System.out.print("Introduce valor 1:");
+        int a = leerEntero(entrada);//entrada.nextInt();
+        System.out.print("Introduce valor 2:");
+        int b = leerEntero(entrada);
+        int resultado = a/b;
 
+        int enteros[]={10,20,30,40};
+
+        System.out.println(enteros[3]);
+                
+        System.out.println(resultado);
+                 
+    }
+    public static int leerEntero(Scanner scanner){
+        while (true){
+            try{
+                int valor = scanner.nextInt();
+                scanner.nextLine(); //consume la linea del enter
+                return valor;
+            }catch(InputMismatchException e){
+                System.out.println("\u001B[31m\t***Error: debe ingresar un número entero válido***\u001B[0m");
+                scanner.nextLine();
+            }
         }
     }
-
 }
+
+
